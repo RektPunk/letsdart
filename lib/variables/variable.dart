a28956e281b91053000bd1cf1119888d72119190
RektPunk <rektpunk@gmail.com>
2022-08-30 00:41:57 +0900

    feat - add nullable variable

--- lib/variables/variable.dart
+++ lib/variables/variable.dart
@@ -3,8 +3,7 @@ void main() {
   var varName = 1; 
   var varStringName = "hi";
   String strVariableName = 'string';
-  String? strNullableVariableName = 'string'; // nullable
-  strNullableVariableName = null;
+  String? strNullableVariableName; // nullable
   int intName = 123;
   intName--; // intName = intName - 1
   intName++; // intName = intName + 1
@@ -13,14 +12,18 @@ void main() {
   bool isTrue = 4 >= 10;
   bool isTrue2 = (4 == 4) | (10 != 10);
   bool isTrue3 = (4 == 4) && (intName == 123);
+  bool isTrue4 = (intName is String) && (4 <= 10);
   print('varName: $varName');
   print('varStringName: $varStringName');
   print('strVariableName: $strVariableName');
   print('strNullableVariableName: $strNullableVariableName');
+  print('nullablevariable: ${strNullableVariableName?.toUpperCase()}');
+  // print('nullablevariable: ${strNullableVariableName!.toUpperCase()}'); // not null
   print('intName: $intName');
   print('doubleName: $doubleName');
   print('boolName: $boolName');
   print('isTrue: $isTrue');
   print('isTrue2: $isTrue2');
   print('isTrue3: $isTrue3');
+  print('isTrue4: $isTrue4');
 }
